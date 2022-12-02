e64c81f934e172a1fca853f4cbbf5ffbed6e017b
paulryanmc <46457961+paulryanmc@users.noreply.github.com>
2022-12-02 05:45:46 -0500

    Delete blockssolo.h

--- blockssolo.h
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <time.h>
-#include <string>
-#include <iostream> 
-
-using namespace sf;
-
-void blockssolo(int vol, int player1Rotate, int player1Left, int player1Right, int player1Drop);
